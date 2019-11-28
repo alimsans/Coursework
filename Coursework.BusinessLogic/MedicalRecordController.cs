2ba83c491e95d85a37da5e305eaf9e9ce46620a2
Alim Yuzbashev <alimsans@gmail.com>
2019-11-29 00:53:22 +0200

    Ту

---
+++ Coursework.BusinessLogic/MedicalRecordController.cs
@@ -0,0 +1,61 @@
+﻿using Coursework.DAL;
+using Coursework.Types;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coursework.BLL
+{
+    public class MedicalRecordController : Controller
+    {
+        public MedicalRecordController()
+        {
+        }
+
+        public MedicalRecordController(DbContextOptions<HospitalContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Adds a medical record to the db
+        /// </summary>
+        /// <param name="record">record to be added</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void AddMedicalRecord(MedicalRecord record)
+        {
+            if (record == null) throw new ArgumentNullException(nameof(record));
+
+            _context.MedicalRecords.Add(record);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Removes a medical record from the db
+        /// </summary>
+        /// <param name="record">record to be removed</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void RemoveMedicalRecord(MedicalRecord record)
+        {
+            if (record == null) throw new ArgumentNullException(nameof(record));
+
+            _context.MedicalRecords.Remove(record);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Gets all patient's medical records
+        /// </summary>
+        /// <param name="patient">Patient whose records to retrieve</param>
+        /// <returns>Collection of medical records</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public ICollection<MedicalRecord> GetPatientsMedicalRecords(Patient patient)
+        {
+            if (patient == null) throw new ArgumentNullException(nameof(patient));
+
+            return _context.MedicalRecords.AsNoTracking().Where(m => m.Patient == patient).ToList();
+        }
+    }
+}
---
+++ Coursework.Tests/MedicalRecordControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Coursework.BLL;
+using Coursework.DAL;
+using Coursework.Types;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Coursework.Tests
+{
+    public class MedicalRecordControllerTests : IDisposable
+    {
+        private DbContextOptions<HospitalContext> _options;
+        private MedicalRecordController _controller;
+
+        public MedicalRecordControllerTests()
+        {
+            Random random = new Random();
+            this._options = new DbContextOptionsBuilder<HospitalContext>()
+                .UseInMemoryDatabase(databaseName: "HospitalTestDb" + random.Next(int.MinValue, int.MaxValue))
+                .Options;
+
+            this._controller = new MedicalRecordController(this._options);
+        }
+
+        public void Dispose()
+        {
+            this._controller.DropDatabase();
+            this._controller.Dispose();
+        }
+
+        [Fact]
+        public void AddMedicalRecord_ShouldAdd()
+        {
+            Patient patient = new Patient("Foo", "Numb");
+            MedicalRecord expected = new MedicalRecord(patient, DateTime.Parse("2019-01-01"), DateTime.Now, "Cancer");
+
+            _controller.AddMedicalRecord(expected);
+
+            var actual = (List<MedicalRecord>)_controller.GetPatientsMedicalRecords(patient);
+
+            Assert.Contains(expected, actual);
+        }
+
+        [Fact]
+        public void AddMedicalRecord_ShouldThrowArgNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _controller.AddMedicalRecord(null));
+        }
+    }
+}
